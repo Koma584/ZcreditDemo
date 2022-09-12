fb10e3088884f0a2f023c69798584fc72417ad1f
Kirklund Brown <45671310+Koma584@users.noreply.github.com>
2022-09-12 15:09:04 -0700

    Update index.js

--- ZcreditDemo/index.js
+++ ZcreditDemo/index.js
@@ -2,11 +2,9 @@ let name = window.prompt("What is your name?");
 let firstAnswer = window.prompt("Are you looking to lend or borrow?");
 if (firstAnswer === "lend") {
   let secondAnswer = window.prompt("how much money are you willing to lend?");
-  alert(secondAnswer, "will be lent to the first person who needs it!");
+  alert(secondAnswer+" will be lent to the first person who needs it!");
 } else if (firstAnswer === "borrow") {
   let secondAnswer = window.prompt("how much money are you looking to borrow?");
-  alert(
-    secondAnswer,
-    "will be received from <user> and is due to be returned by <date>"
+  alert(secondAnswer+" will be received from <user> and is due to be returned by <date>"
   );
 }
